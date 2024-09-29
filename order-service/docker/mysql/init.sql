ef6ad4a9cf5a1cb7a93ede5d4d5e7133b9823eae
martin-ngigi <martinwainaina001@gmail.com>
2024-09-29 09:31:55 +0300

    Added Inventory Service

--- order-service/docker/mysql/init.sql
+++ order-service/docker/mysql/init.sql
@@ -1,18 +1,1 @@
-
 CREATE DATABASE IF NOT EXISTS order_service;
-
---USE order_service;
---
---CREATE TABLE IF NOT EXISTS orders (
---    id INT AUTO_INCREMENT PRIMARY KEY,
---    customer_name VARCHAR(100) NOT NULL,
---    product_name VARCHAR(100) NOT NULL,
---    quantity INT NOT NULL,
---    order_date DATETIME DEFAULT CURRENT_TIMESTAMP
---);
-
---
---INSERT INTO orders (customer_name, product_name, quantity) VALUES
---('John Doe', 'Laptop', 1),
---('Jane Smith', 'Smartphone', 2),
---('Alice Johnson', 'Tablet', 3);
