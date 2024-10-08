ecd763959aa4a2525e6621a50d93c6d608108dc6
martin-ngigi <martinwainaina001@gmail.com>
2024-10-08 11:06:23 +0300

    Integrated Order Microservice with Inventory Microservice

--- inventory-service/src/main/java/com/safiribytes/shoppingmicroservice/inventory_service/Service/InventoryService.java
+++ inventory-service/src/main/java/com/safiribytes/shoppingmicroservice/inventory_service/Service/InventoryService.java
@@ -1,8 +1,11 @@
 package com.safiribytes.shoppingmicroservice.inventory_service.Service;
 
+import com.safiribytes.shoppingmicroservice.inventory_service.model.Inventory;
 import com.safiribytes.shoppingmicroservice.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +15,11 @@ public class InventoryService {
 
     public boolean isInStore(String skuCode, Integer quantity){
         return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+
+    // Method to get all products
+    public List<Inventory> getAllProducts() {
+        return inventoryRepository.findAll();
     }
 
 }
--- inventory-service/src/main/java/com/safiribytes/shoppingmicroservice/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/safiribytes/shoppingmicroservice/inventory_service/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.safiribytes.shoppingmicroservice.inventory_service.controller;
 
 import com.safiribytes.shoppingmicroservice.inventory_service.Service.InventoryService;
+import com.safiribytes.shoppingmicroservice.inventory_service.model.Inventory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -16,5 +19,12 @@ public class InventoryController {
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity){
         return inventoryService.isInStore(skuCode, quantity);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping("/all/products")
+    public List<Inventory> getAllProducts(){
+        return inventoryService.getAllProducts();
     }
 }
