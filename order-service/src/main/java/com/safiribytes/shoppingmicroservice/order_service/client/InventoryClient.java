348f6bbeba6ca63f422ceadb28f8fabe3601ee1c
martin-ngigi <martinwainaina001@gmail.com>
2024-10-14 15:49:39 +0300

    Replaced OpenFeign with rest client

--- order-service/src/main/java/com/safiribytes/shoppingmicroservice/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/safiribytes/shoppingmicroservice/order_service/client/InventoryClient.java
@@ -1,15 +1,14 @@
 package com.safiribytes.shoppingmicroservice.order_service.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-//@FeignClient(value = "inventory", url = "http://localhost:8082")
-@FeignClient(value = "inventory", url = "${inventory.url}")
+/// Communication between two services. i.e. between order-service and inventory-service
 public interface InventoryClient {
 
-    /// Feign will add the necessary implementation
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    /// Its implementation is in config/RestClientConfig
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
